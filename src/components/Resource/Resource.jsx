2c42df49b02f6bba820a7adc2ba8d0e203be2c46
Pape Mayel Diagne THIAM <97792012+mayel15@users.noreply.github.com>
2023-08-10 12:29:27 +0200

    fix the font size

--- src/components/Resource/Resource.jsx
+++ src/components/Resource/Resource.jsx
@@ -121,7 +121,7 @@ export default function Resource(props) {
         <div className="resource-card col-lg-12">
             <h3> <a href={props.url} target="blank">{props.title}</a></h3>
             <div>{props.description}</div>
-            <div>Auteurs / Sources : {props.authors}</div>
+            <div>Auteurs/Sources : {props.authors}</div>
             <div>{props.subject + " > " + props.category}</div>
             <p>{props.addedAt}</p>
             {
