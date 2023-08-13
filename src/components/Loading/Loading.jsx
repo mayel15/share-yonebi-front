1888f870cea2a55fb49b2efd81145cbec4d99c14
Pape Mayel Diagne THIAM <97792012+mayel15@users.noreply.github.com>
2023-08-14 01:44:56 +0200

    add background image

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -4,7 +4,7 @@ import './Loading.css'
 export default function Loading (props){
     return(
         <div className="loading-img-container">
-            <img className="loading-img" src={props.url} alt='loading-img'></img>
+            <img className="loading-img" src={props.url} alt='loading-img'/>
         </div>
     )
 } 
